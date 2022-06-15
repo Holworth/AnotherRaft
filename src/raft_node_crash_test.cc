8c0a798a49aeae3b490ff3ec83997cbf6d35ebdf
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-06-15 21:36:40 +0800

    Add more complex test

--- src/raft_node_crash_test.cc
+++ src/raft_node_crash_test.cc
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <unordered_map>
 
+#include "RCF/InitDeinit.hpp"
 #include "raft_node_test.h"
 #include "raft_type.h"
 namespace raft {
@@ -20,6 +21,13 @@ class RaftNodeCrashTest : public RaftNodeTest {
     auto net_config = GetNetConfigFromNodesConfig(config);
     auto node_config = config.at(id);
     LaunchRaftNodeInstance({id, net_config, node_config.storage_name, new RsmMock});
+  }
+
+  void Restart(const NodesConfig& config, raft_node_id_t id) {
+    if (nodes_[id] != nullptr) {
+      Crash(id);
+    }
+    Reboot(config, id);
   }
 
   void PreRemoveFiles(const NodesConfig& config) {
@@ -58,5 +66,80 @@ TEST_F(RaftNodeCrashTest, TestSimpleRestartLeader) {
   EXPECT_TRUE(ProposeOneEntry(104));
 
   ClearTestContext(config);
+}
+
+TEST_F(RaftNodeCrashTest, TestSomeCrashOccurs) {
+  auto config = ConstructNodesConfig(3, true);
+  PreRemoveFiles(config);
+
+  LaunchAllServers(config);
+  sleepMs(10);
+
+  EXPECT_TRUE(ProposeOneEntry(101));
+  
+  for (int i = 0; i < node_num_; ++i) {
+    Restart(config, i);
+  }
+
+  EXPECT_TRUE(ProposeOneEntry(102));
+
+  auto leader1 = GetLeaderId();
+  Disconnect(leader1);
+  Restart(config, leader1);
+
+  EXPECT_TRUE(ProposeOneEntry(103));
+
+  auto leader2 = GetLeaderId();
+  Disconnect(leader2);
+  EXPECT_TRUE(ProposeOneEntry(104));
+  Restart(config, leader2);
+
+  sleepMs(1000);
+
+  auto i3 = (GetLeaderId() + 1) % node_num_;
+  Disconnect(i3);
+  EXPECT_TRUE(ProposeOneEntry(105));
+  Restart(config, i3);
+
+  EXPECT_TRUE(ProposeOneEntry(106));
+
+  ClearTestContext(config);
+}
+
+TEST_F(RaftNodeCrashTest, TestManyCrashOccurs) {
+  auto config = ConstructNodesConfig(5, true);
+  PreRemoveFiles(config);
+
+  LaunchAllServers(config);
+  sleepMs(10);
+
+  const int iter_cnt = 5;
+  int init_val = 1;
+  for (int iter = 0; iter < iter_cnt; ++iter) {
+    EXPECT_TRUE(ProposeOneEntry(init_val));
+    ++init_val;
+
+    auto leader1 = GetLeaderId();
+
+    Disconnect((leader1 + 1) % node_num_);
+    Disconnect((leader1 + 2) % node_num_);
+
+    EXPECT_TRUE(ProposeOneEntry(init_val));
+    ++init_val;
+
+    Disconnect((leader1 + 3) % node_num_);
+    Disconnect((leader1 + 4) % node_num_);
+    Disconnect((leader1 + 0) % node_num_);
+
+    Restart(config, (leader1 + 1) % node_num_);
+    Restart(config, (leader1 + 2) % node_num_);
+    Restart(config, (leader1 + 3) % node_num_);
+
+    EXPECT_TRUE(ProposeOneEntry(init_val));
+    ++init_val;
+
+    Reconnect((leader1 + 0) % node_num_);
+    Reconnect((leader1 + 4) % node_num_);
+  }
 }
 }  // namespace raft
