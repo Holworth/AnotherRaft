41f925fa60d2314c957d30ece9623a615c704a4f
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-08-04 22:02:25 +0800

    Add script for automatically install dependencies

---
+++ scripts/install_dependencies.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+def install_rocksdb():
+    if not os.path.exists("v7.4.5.tar.gz"):
+        subprocess.run("wget https://github.com/facebook/rocksdb/archive/refs/tags/v7.4.5.tar.gz", shell=True)
+    if not os.path.exists("rocksdb-7.4.5"):
+        subprocess.run("tar -zxvf v7.4.5.tar.gz", shell=True)
+    os.chdir("rocksdb-7.4.5")
+    subprocess.run("make static_lib", shell=True)
+    subprocess.run("sudo make install", shell=True)
+    os.chdir("..")
+    subprocess.run("rm -rf rocksdbv7.4.5 && rm -rf v7.4.5.tar.gz", shell=True)
+
+def install_rocksdb_dependencies():
+    subprocess.run("sudo yum makecache")
+    subprocess.run("sudo yum -y install zlib-devel", shell=True)
+    subprocess.run("sudo yum -y install bzip2", shell=True)
+    subprocess.run("sudo yum -y install lz4", shell=True)
+    subprocess.run("sudo yum -y install snappy", shell=True)
+    subprocess.run("sudo yum -y install zstd", shell=True)
+
+
+if __name__ == "__main__":
+    install_rocksdb_dependencies()
+    install_rocksdb()
+
+
