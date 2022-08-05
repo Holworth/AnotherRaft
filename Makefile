0b552dcb55e02ef790436864c7f4febcdc17f1af
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-08-05 20:13:17 +0800

    Enable multi-thread compilation

--- Makefile
+++ Makefile
@@ -2,19 +2,19 @@
 release: clean 
 	${CMAKE} -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=true -DLOG=off
 	mv build/compile_commands.json ./
-	${CMAKE} --build build
+	${CMAKE} --build build -j 4
 
 .PHONY: build
 build: clean 
 	${CMAKE} -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=true -DLOG=off
 	mv build/compile_commands.json ./
-	${CMAKE} --build build
+	${CMAKE} --build build -j 4
 
 .PHONY: log
 log: clean 
 	${CMAKE} -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=true -DLOG=on
 	mv build/compile_commands.json ./
-	${CMAKE} --build build
+	${CMAKE} --build build -j 4
 
 .PHONY: clean
 clean:
--- scripts/run_bench.py
+++ scripts/run_bench.py
@@ -2,57 +2,69 @@ import paramiko
 from typing import List
 
 class Server:
-    def __init__(self, ip, port, username, passwd) -> None:
+    def __init__(self, ip, port, username, passwd, id) -> None:
         self.ip = ip
         self.port = port
         self.username = username
         self.passwd = passwd
+        self.id = id
 
-def ssh2(ip,username,passwd,cmd):
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip,22,username,passwd,timeout=5)
-        stdin,stdout,stderr = ssh.exec_command(cmd)
-        # stdin.write("Y") #interact with server, typing Y 
-        print(stdout.read())
-        # for x in stdout.readlines():
-        # print x.strip("n")
-        print('%s OK\n'%(ip))
-        ssh.close()
-    except :
-        print('%s Error\n' %(ip))
-        
 
 def build_executable(servers: List[Server]):
-
     commands = [
         "cd /home/kangqihan",
         "rm -rf AnotherRaft",
         "git clone kqh:Holworth/AnotherRaft.git",
         "cd AnotherRaft",
         "CMAKE=/usr/bin/cmake3 scl enable devtoolset-10 \"make build\""
-        # "cd AnotherRaft",
-        # "mkdir build && cd build",
-        # "cmake3 .. -DCMAKE_BUILD_TYPE=Debug -DLOG=off",
-        # "make bench_server && make bench_client"
     ]
-
     ssh_cmd = ""
     for cmd in commands:
         ssh_cmd = ssh_cmd + cmd + ";"
-    print(ssh_cmd)
+    # print(ssh_cmd)
 
     for server in servers:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(server.ip,22,server.username,server.passwd,timeout=5)
         stdin, stdout, stderr = ssh.exec_command(ssh_cmd)
-        print(stdout.readlines())
+        stdout.read()
         ssh.close()
+    print("Finish Build Executable File on Servers")
+
+def run_kv_server(server: Server):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server.ip,22,server.username,server.passwd,timeout=5)
+
+    cmd = "cd /home/kangqihan/AnotherRaft/build; bench/bench_server ../bench/cluster.cfg " + str(server.id) + "&"
+    stdin, stdout, stderr = ssh.exec_command(cmd);
+    stdout.read()
+    ssh.close()
+
+    print("[KvServer %d] starts up", server.id)
+
+def run_kv_client(server: Server, clientid: int, valueSize: str, putCnt:int):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(server.ip,22,server.username,server.passwd,timeout=5)
+    cmd = "cd /home/kangqihan/AnotherRaft/build; \
+           bench/bench_client ../bench/cluster.cfg {} {} {}".format(clientid, valueSize, putCnt)
+    print("Execute client command {}".format(cmd))
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    print(stdout.readlines())
+    ssh.close()
+    
+
 
 if __name__ == "__main__":
-    build_executable([Server("10.118.0.40", "22", "root", "ict#96")])
-        
-    
-    
+    servers = [
+        Server("10.118.0.40", "22", "root", "ict#96", 0),
+        Server("10.118.0.42", "22", "root", "ict#96", 1),
+        Server("10.118.0.48", "22", "root", "1357246$", 2),
+        Server("10.118.0.49", "22", "root", "1357246$", 3)
+    ]
+    build_executable(servers)
+    for server in servers[:-1]:
+        run_kv_server(server)
+    run_kv_client(servers[-1], 0, "4K", 1000)
