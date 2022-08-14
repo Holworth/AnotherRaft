b146388ae11c1826048a7a3f9475c7e43c55adcf
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-08-14 21:06:34 +0800

    Push by kqh

--- scripts/cloud_runbench.py
+++ scripts/cloud_runbench.py
@@ -15,16 +15,17 @@ class Server:
         self.id = id
 
 class BenchmarkConfiguration:
-    def __init__(self, client_id, value_size, put_cnt, servers) -> None:
+    def __init__(self, client_id, value_size, put_cnt, servers, cfg_file) -> None:
         self.client_id = client_id
         self.value_size = value_size
         self.put_cnt = put_cnt
         self.servers = servers
+        self.cfg_file = cfg_file
 
 
 
-def run_kv_server(server: Server) -> int:
-    cmd = "cd /home/kangqihan/AnotherRaft/build; nohup bench/bench_server ../bench/cloud_cluster3.cfg " + str(server.id) + " > /dev/null 2> /dev/null &"
+def run_kv_server(server: Server, cfg: BenchmarkConfiguration) -> int:
+    cmd = "cd /home/kangqihan/AnotherRaft/build; nohup bench/bench_server ../bench/{} ".format(cfg.cfg_file) + str(server.id) + " > /dev/null 2> /dev/null &"
     ssh_cmd = "ssh -i /root/.ssh/FlexibleK_Experiment.pem {}@{}".format(server.username, server.ip) + " \"" + cmd + "\""
     pr = subprocess.run(ssh_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
     if pr.returncode != 0:
@@ -34,9 +35,9 @@ def run_kv_server(server: Server) -> int:
         print("[KvServer {}] starts up".format(server.id))
         return 0
 
-def run_kv_client(server: Server, clientid: int, valueSize: str, putCnt:int) -> int:
+def run_kv_client(server: Server, clientid: int, valueSize: str, putCnt:int, cfg: BenchmarkConfiguration) -> int:
     cmd = "cd /home/kangqihan/AnotherRaft/build; \
-           bench/bench_client ../bench/cloud_cluster3.cfg {} {} {}".format(clientid, valueSize, putCnt)
+           bench/bench_client ../bench/{} {} {} {}".format(cfg.cfg_file, clientid, valueSize, putCnt)
     ssh_cmd = "ssh -i /root/.ssh/FlexibleK_Experiment.pem {}@{}".format(server.username, server.ip) + " \"" + cmd + "\""
 
     f = open("./results", "a")
@@ -76,13 +77,13 @@ def stop_kv_server(server: Server):
 
 def run_benchmark(config: BenchmarkConfiguration) -> int:
     for server in config.servers[:-1]:
-        r = run_kv_server(server)
+        r = run_kv_server(server, config)
         if r != 0:
             stop_kv_servers(config.servers[:-1])
             print("[Failed to launch server{}, kill all servers, Retry]".format(server.id))
             return r
 
-    r = run_kv_client(config.servers[-1], config.client_id, config.value_size, config.put_cnt)
+    r = run_kv_client(config.servers[-1], config.client_id, config.value_size, config.put_cnt, config)
     stop_kv_servers(config.servers[:-1])
     if r != 0:
         print("[Failed to execute client, kill all servers, Retry]")
@@ -101,6 +102,14 @@ def run_benchmark_succ(config: BenchmarkConfiguration):
 
 
 if __name__ == "__main__":
+    cfg_file = ""
+    if len(sys.argv) < 2:
+        cfg_file = "cloud_cluster5.cfg"
+        print("[Expect one configuration file as parameter, got none, use default: {}]".format(cfg_file))
+    else:
+        cfg_file = sys.argv[1]
+        print("[Use input configuration file: {}]".format(cfg_file))
+
     servers = [
         Server("172.20.126.134", "22", "root", "", 0),
         Server("172.20.126.135", "22", "root", "", 1),
@@ -111,16 +120,16 @@ if __name__ == "__main__":
     ]
 
     cfgs = [
-        BenchmarkConfiguration(0, "4K", 10000, servers),
-        BenchmarkConfiguration(0, "8K", 10000, servers),
-        BenchmarkConfiguration(0, "16K", 10000, servers),
-        BenchmarkConfiguration(0, "32K", 10000, servers),
-        BenchmarkConfiguration(0, "64K", 10000, servers),
-        BenchmarkConfiguration(0, "128K", 10000, servers),
-        BenchmarkConfiguration(0, "256K", 10000, servers),
-        BenchmarkConfiguration(0, "512K", 10000, servers),
-        BenchmarkConfiguration(0, "1M", 10000, servers),
-        BenchmarkConfiguration(0, "2M", 10000, servers),
+        BenchmarkConfiguration(0, "4K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "8K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "16K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "32K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "64K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "128K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "256K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "512K", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "1M", 10000, servers, cfg_file),
+        BenchmarkConfiguration(0, "2M", 10000, servers, cfg_file),
     ]
 
     for cfg in cfgs:
